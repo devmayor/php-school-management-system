c800e4cd8fadb7605af4671ab81aec8428157104
G2G2G2G <50514477+G2G2G2G@users.noreply.github.com>
2020-09-03 02:06:52 -0500

    learn to code

--- session.php
+++ session.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
 if(!empty($_SESSION['adminuser'])){
-
-if(!empty($_SESSION['adminuser'])){
-$adminuser=$_SESSION['adminuser'];
-}
-
+  $adminuser=$_SESSION['adminuser'];
 }else{
-header('location:index.php');
+  header('location:index.php');
 }
-
-
 ?>
